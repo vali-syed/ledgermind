265056b5ed03adb5d53b734dad9a77d24eb1dcaf
vali-syed <chinnavalisyed@gmail.com>
2026-07-19 21:57:27 +0530

    CORS origin error fixed

--- server/app/main.py
+++ server/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ledgermind-five.vercel.app/"],
+    allow_origins=["https://ledgermind-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
